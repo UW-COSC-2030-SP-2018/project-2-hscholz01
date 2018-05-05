7ee15b2c8ea26078ce6debf8d663c820ba2b33f2
hscholz01 <hscholz@uwyo.edu>
2018-05-04 23:45:38 -0600

    Binary search and Merge sort added

--- Functions.h
+++ Functions.h
@@ -1,55 +1,160 @@
 #pragma once
 
-#include <iostream>
 #include <vector>
+#include "LinkedList.h"
 
 using namespace std;
 
-template<class T> void Quicksort(vector<T> vec, vector<T>::iterator begin, vector<T>::iterator end);
-template<class T> void quicksort(vector<T> vec);
-template<class T> void swap(T A, T B);
-
 template<class T>
-	void swap(T A, T B)
+vector<T> Quicksort(vector<T> vec, int start, int stop)
 {
-	T temp = A;
-	A = B;
-	B = temp;
-}
-
-template<class T>
-	void Quicksort(vector<T> vec, vector<T>::iterator left, vector<T>::iterator right)
-{
-	if (vec.size() > 1)
+	if ((stop - start) >= 1)
 	{
-		T pivot = vec[vec.size() / 2];
+		int left = start;
+		int right = stop;
+		T pivot = vec[left + (((right + 1) - left) / 2)];
 
 		while (left <= right)
 		{
-			while (*left < pivot)
+			while (vec[left] < pivot && left <= stop)
 			{
-				left++;
+				left = left + 1;
 			}
-			while (*right > pivot)
+
+			while (vec[right] > pivot && right >= start)
 			{
-				right--;
+				right = right - 1;
 			}
+
 			if (left <= right)
 			{
-				swap(*left, *right);
+				T temp = vec[right];
+				vec[right] = vec[left];
+				vec[left] = temp;
 
-				left++;
-				right--;
+				left = left + 1;
+				right = right - 1;
 			}
 		}
+		
+		vec = Quicksort(vec, start, right);
 
-		Quicksort(vec.begin(), vec.begin() + right);
-		Quicksort(vec.begin() + left, vec.end());
+		vec = Quicksort(vec, left, stop);
+	}
+
+	return vec;
+}
+
+template<class T>
+vector<T> quicksort(vector<T> vec)
+{
+		return Quicksort<T>(vec, 0, vec.size() - 1);
+}
+
+template<class T>
+T BinarySearch(typename vector<T> vec, typename T value, int low, int high)
+{
+	if (high < low)
+	{
+		return NULL;
+	}
+
+	int mid = low + ((high - low) / 2);
+
+	if (vec[mid] > value)
+	{
+		return BinarySearch(vec, value, low, mid - 1);
+	}
+
+	else if (vec[mid] < value)
+	{
+		return BinarySearch(vec, value, mid + 1, high);
+	}
+
+	else
+	{
+		return mid;
 	}
 }
 
 template<class T>
-	void quicksort(vector<T> vec)
+T binarySearch(typename vector<T> vec, typename T value)
 {
-		Quicksort(vec, vec.begin(), vec.end());
+	return BinarySearch(vec, value, 0, vec.size() - 1);
+}
+
+template<class T>
+vector<T> MergeSort(typename vector<T> vec)
+{
+	vector<T> left, right, result;
+	if ((vec.size()) <= 1)
+	{
+		return vec;
+	}
+	else
+	{
+		int middle = (vec.size()) / 2;
+
+		for (int i = 0; i <= middle - 1; i++)
+		{
+			left.push_back(vec[i]);
+		}
+
+		for (int j = middle; j <= vec.size() - 1; j++)
+		{
+			right.push_back(vec[j]);
+		}
+
+		left = MergeSort<T>(left);
+		right = MergeSort<T>(right);
+
+		if (left[left.size() - 1] <= right[0])
+		{
+			left.insert(left.end(), right.begin(), right.end());
+			return left;
+		}
+
+		result = merge<T>(left, right);
+		return result;
+	}
+}
+
+template<class T>
+vector<T> merge(vector<T> left, vector<T> right)
+{
+	vector<T> result;
+	int leftInd = 0;
+	int rightInd = 0;
+
+	while ((left.size()) > leftInd && (right.size()) > rightInd)
+	{
+		if (left[leftInd] <= right[rightInd])
+		{
+			result.push_back(left[leftInd]);
+			leftInd++;
+		}
+		else
+		{
+			result.push_back(right[rightInd]);
+			rightInd++;
+		}
+	}
+	if (leftInd < left.size())
+	{
+		result.insert(result.end(), left.begin() + leftInd, left.end());
+	}
+	if (rightInd < right.size())
+	{
+		result.insert(result.end(), right.begin() + rightInd, right.end());
+	}
+	
+	return result;
+}
+
+template<class T>
+int Hash(typename T info)
+{
+	List bigList;
+
+
 }
--- Tester.cpp
+++ Tester.cpp
@@ -1,26 +1,240 @@
 #include <iostream>
+#include <ctime>		//only used as seed for rand()
 #include <vector>
 #include "Functions.h"
+#include "winTimer.h"
 
 int main()
 {
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// Quicksort with a vector of ints
+	///////////////////////////////////////////////////////////////////////////////////////////////
 
-	int myInts[] = {3, 5, 7, 2, 1, 9, 4, 3, 7, 9};
-	vector<int> testVec;
+	cout << "\n\n--------------------Quicksort with a vector of ints--------------------\n";
+	cout << "\nWhen the vector is of 100 random ints between 0 and 99:\n";
 
-	testVec.assign(myInts, myInts + 10);
-
+	vector<int> testVec(100);
 	for (vector<int>::iterator it1 = testVec.begin(); it1 != testVec.end(); ++it1)
 	{
-		cout << *it1 << " ";
+		*it1 = rand() % 100;
 	}
-	cout << endl;
 
-	quicksort<int>(testVec);
+	Timer timer1;
 
-	for (vector<int>::iterator it2 = testVec.begin(); it2 != testVec.end(); ++it2)
+	timer1.start();
+	testVec = quicksort<int>(testVec);
+	timer1.stop();
+
+	cout << "\n" << timer1() << " seconds.\n";
+
+	cout << "\nif the size is quadrupled:\n";
+	testVec = vector<int>(400);
+	for (vector<int>::iterator it400 = testVec.begin(); it400 != testVec.end(); ++it400)
 	{
-		cout << *it2 << " ";
+		*it400 = rand() % 100;
 	}
-	cout << endl;
+
+	timer1.clear();
+	timer1.start();
+	testVec = quicksort<int>(testVec);
+	timer1.stop();
+
+	cout << "\n" << timer1() << " seconds.\n";
+
+
+	cout << "\nIf the vector is sorted in reverse order from 25 to 0:\n";
+	testVec = { 25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1 };
+
+	timer1.clear();
+	timer1.start();
+	testVec = quicksort(testVec);
+	timer1.stop();
+
+	cout << "\n" << timer1() << " seconds.\n";
+
+	cout << "\nIf the vector is of size 100 and all the same value:\n";
+	testVec = vector<int>(100, 1);
+
+	timer1.clear();
+	timer1.start();
+	testVec = quicksort(testVec);
+	timer1.stop();
+
+	cout << "\n" << timer1() << " seconds.\n";
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// Quicksort with a string as a vector of chars
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	cout << "\n\n--------------------Quicksort with a string as a vector of chars--------------------\n";
+	std::string sentence = "the quick brown fox jumps over the lazy dog";
+	vector<char> test2(sentence.begin(), sentence.end());
+
+	cout << "\nthe string is:\n";
+	for (vector<char>::iterator it1 = test2.begin(); it1 != test2.end(); ++it1)
+	{
+		cout << *it1;
+	}
+	cout << '.' <<  endl;
+
+	Timer timer2;
+
+	timer2.start();
+	test2 = quicksort<char>(test2);
+	timer2.stop();
+
+	cout << "\nWhen the string is sorted into a vector using quicksort it becomes:\n" << '{';
+	for (vector<char>::iterator it2 = test2.begin(); it2 != test2.end(); ++it2)
+	{
+		cout << *it2;
+	}
+	cout << '}' << endl;
+
+	cout << "\nThis operation took " << timer2() << " seconds.\n";
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// Binary Search thrugh a vector of ints
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	cout << "\n\n--------------------Binary Search with a vector of ints--------------------\n";
+	cout << "\nFor a reversely ordered list from 0 to 99 and looking for the value 73:\n";
+
+	int inc = 0;
+	vector<int> test3(100);
+	for (vector<int>::iterator it6 = test3.begin(); it6 != test3.end(); ++it6)
+	{
+		*it6 = inc;
+		inc++;
+	}
+
+	Timer timer3;
+	int value = 73;
+	int loc;
+	
+	timer3.start();
+	loc = binarySearch(test3, value);
+	timer3.stop();
+
+	cout << "\nThis operation took " << timer3() << " seconds.\n";
+
+	cout << "\nIf the length of the vector is quadrupled:\n";
+	inc = 0;
+	test3 = vector<int>(400);
+	for (vector<int>::iterator it6 = test3.begin(); it6 != test3.end(); ++it6)
+	{
+		*it6 = inc;
+		inc++;
+	}
+
+	timer3.clear();
+	timer3.start();
+	loc = binarySearch<int>(test3, value);
+	timer3.stop();
+
+	cout << "\nThis operation took " << timer3() << " seconds.\n";
+
+	cout << "\nIf the value is not in the vector:\n";
+	test3 = vector<int>(100, 1);
+
+	timer3.clear();
+	timer3.start();
+	loc = binarySearch<int>(test3, value);
+	timer3.stop();
+
+	cout << "\nThis operation took " << timer3() << " seconds.\n";
+
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Binary search through a vector of chars
+	//////////////////////////////////////////////////////////////////////////////////////////
+
+	cout << "\n\n--------------------Binary Search with a vector of chars--------------------\n";
+	cout << "\nIf the vector is the alphabet and the value is 'v':\n";
+
+	vector<char> test4({ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' });
+	char c = 'v';
+
+	Timer timer4;
+
+	timer4.start();
+	loc = binarySearch<char>(test4, c);
+	timer4.stop();
+
+	cout << "\nThis operation took " << timer4() << " seconds.\n";
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// Merge Sort a vector of doubles
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+	cout << "\n\n--------------------Merge Sort with a vector of doubles--------------------\n";
+	cout << "\nIf the vector consists of 100 random doubles from 0 to 100:\n";
+
+	vector<double> test5(100);
+	for (vector<double>::iterator it7 = test5.begin(); it7 != test5.end(); ++it7)
+	{
+		*it7 = ((double)rand() / RAND_MAX) * 100;
+	}
+	/*
+	cout << "\nBefore MergeSort:\n" << '{';
+	for (vector<double>::iterator it9 = test5.begin(); it9 != test5.end(); ++it9)
+	{
+		cout << *it9 << ',';
+	}
+	cout << '}' << endl;
+	*/
+	Timer timer5;
+
+	timer5.start();
+	test5 = MergeSort<double>(test5);
+	timer5.stop();
+	/*
+	cout << "\nAfter MergeSort:\n" << '{';
+	for (vector<double>::iterator it8 = test5.begin(); it8 != test5.end(); ++it8)
+	{
+		cout << *it8 << ',';
+	}
+	cout << '}' << endl;
+	*/
+	cout << "\nThis operation took " << timer5() << " seconds.\n";
+
+	cout << "\nIf the values in the vector are all the same:\n";
+
+	for (vector<double>::iterator it11 = test5.begin(); it11 != test5.end(); ++it11)
+	{
+		*it11 = 0.0;
+	}
+
+	timer5.clear();
+	timer5.start();
+	test5 = MergeSort<double>(test5);
+	timer5.stop();
+
+	cout << "\nThis operation took " << timer5() << " seconds.\n";
+
+	cout << "\nIf the values in the vector are reverse sorted:\n";
+
+	double dec = 100.0;
+	for (vector<double>::iterator it12 = test5.begin(); it12 != test5.end(); ++it12)
+	{
+		*it12 = dec;
+		dec--;
+	}
+
+	timer5.clear();
+	timer5.start();
+	test5 = MergeSort<double>(test5);
+	timer5.stop();
+
+	cout << "\nThis operation took " << timer5() << " seconds.\n";
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Text/Int to Int Hash Function
+	/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+	system("pause");
 }
